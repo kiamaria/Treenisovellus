6f365d949a7ca9fc5c8d626e6e87ce77c2acbf5a
kiamaria <123375780+kiamaria@users.noreply.github.com>
2024-04-10 19:13:23 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeAuth, getReactNativePersistence, getAuth } from "firebase/aut
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDa2IpQz_68fHWR5N5_KQvu5Ehk8igfNwE",
-  authDomain: "treenisovellus.firebaseapp.com",
+  apiKey: "xccv",
+  authDomain: "--firebaseapp.com",
   projectId: "treenisovellus",
   storageBucket: "treenisovellus.appspot.com",
-  messagingSenderId: "124914196786",
-  appId: "1:124914196786:web:b82511e96c97971e9a372b",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
